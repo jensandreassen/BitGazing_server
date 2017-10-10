3988ac28b31b96a23fca4506397ec882582669f1
oscar a <oscar093@live.se>
2017-10-10 14:57:55 +0200

    Market-Beans generering funkgerar.

---
+++ src/main/java/webApp/Market.java
@@ -0,0 +1,17 @@
+package webApp;
+
+public class Market {
+	public String symbol = "";
+	public String currency = "";
+	public double high = -1;
+	public double latest_trade = -1;
+	public double weighted_price = -1;
+	public double bid = -1;
+	public double volume = -1;
+	public double ask = -1;
+	public double low = -1;
+	public double duration = -1;
+	public double close = -1;
+	public double avg = -1;
+	public double currency_volume = -1;
+}
--- src/main/java/webApp/Processer.java
+++ src/main/java/webApp/Processer.java
@@ -1,28 +1,38 @@
 package webApp;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-
 import com.google.gson.Gson;
 
 public class Processer {
-	private Gson g = new Gson();
+	private Gson gson = new Gson();
 	private FileReader fr = new FileReader();
 	private String json;
 	
 	public Processer(){
 		try {
-			this.json = fr.readFile("/home/osc/User_Oscar/Mah/mahSysHT17/Workspace2/BitGazing/files/exampleData.txt");
+			this.json = fr.readFile("files/exampleData.txt");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public String getJson() {
-		return json;
+	public String getJsonString() {
+		return this.json;
 	}
+	
+	public Market[] convertAndGetGsonBean() {
+		Market[] data = gson.fromJson(this.getJsonString(), Market[].class);
+		return data;
+	}
+	
 	//Only for testing!
 	public static void main(String[] args) {
 		Processer p = new Processer();
+		Market[] d = p.convertAndGetGsonBean();
+		for(Market da : d) {
+			System.out.println(da.symbol);
+			System.out.println(da.currency);
+			System.out.println(da.latest_trade + "\n" );
+		}
 	}
 }
