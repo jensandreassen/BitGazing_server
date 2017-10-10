c97e561564982d2134a757f15e804897f2d99d5c
oscar a <oscar093@live.se>
2017-10-10 14:14:18 +0200

    Skapade exampleData.txt

---
+++ src/main/java/webApp/Data.java
@@ -0,0 +1,5 @@
+package webApp;
+
+public class Data {
+
+}
--- src/main/java/webApp/Processer.java
+++ src/main/java/webApp/Processer.java
@@ -1,5 +1,28 @@
 package webApp;
 
-public class Processer {
+import java.io.IOException;
+import java.nio.charset.Charset;
 
+import com.google.gson.Gson;
+
+public class Processer {
+	private Gson g = new Gson();
+	private FileReader fr = new FileReader();
+	private String json;
+	
+	public Processer(){
+		try {
+			this.json = fr.readFile("/home/osc/User_Oscar/Mah/mahSysHT17/Workspace2/BitGazing/files/exampleData.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getJson() {
+		return json;
+	}
+	//Only for testing!
+	public static void main(String[] args) {
+		Processer p = new Processer();
+	}
 }
