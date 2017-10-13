29eace00a044521203f9cb5726cb58df3198d66a
kalle303 <karlparadis96@gmail.com>
2017-10-13 15:24:19 +0200

    Metod för BitcoinAverage i DataFetcher

--- src/main/java/webApp/DataFetcher.java
+++ src/main/java/webApp/DataFetcher.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.json.JSONObject;
 
-import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -13,45 +12,54 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public class DataFetcher {
 	
 	/**
-	 * Hämtar marknads-data som JSON från bitcoincharts.com
+	 * Hämtar marknads-data om en specifik marknad från bitcoinaverage.com
+	 * @param market
+	 * @return
+	 * @throws UnirestException
+	 * @throws IOException
+	 */
+	public static JSONObject fetchBTCMarket(String market) throws UnirestException, IOException {
+		HttpResponse<JsonNode> response = Unirest.get("https://apiv2.bitcoinaverage.com/exchanges/ticker/" + market)
+		.header("accept", "application/json")
+		.asJson();
+		Unirest.shutdown();
+		JSONObject marketData = response.getBody().getObject();
+		return marketData;
+	}
+	
+	/**
+	 * Hämtar all marknads-data från bitcoincharts.com
 	 * Anropa inte mer än 1 gång per 15min!
 	 * @return
 	 * @throws UnirestException
 	 * @throws IOException
 	 */
-	public static JSONObject fetchBitcoinMarketData() throws UnirestException, IOException {
+	public static JSONObject fetchAllBTCMarkets() throws UnirestException, IOException {
 		HttpResponse<JsonNode> response = Unirest.get("http://api.bitcoincharts.com/v1/markets.json")
 		.header("accept", "application/json")
-		.asJson(); // Berätta för Unirest att vi får tillbaka JSON.
-		
-		JsonNode json = response.getBody(); //JSON-objektet ligger gömt som en JsonNode i svaret
-		JSONObject data = json.getObject();
-		
-		// Man måste stänga ner Unirest innan programmet avslutas
+		.asJson();
 		Unirest.shutdown();
-
-		return data;
+		JSONObject allMarkets = response.getBody().getObject();
+		return allMarkets;
 	}
 	
+	/**
+	 * Hämtar valuta-kurser från openexchangerates.org
+	 * @return
+	 * @throws UnirestException
+	 * @throws IOException
+	 */
 	public static JSONObject fetchCurrencyRates() throws UnirestException, IOException {
-		//19c47c8596ba43bd9c7cf51a37928bc3
-		HttpResponse<JsonNode> response;
-		response = Unirest.get("https://openexchangerates.org/api/latest.json")
-		.queryString("app_id", "19c47c8596ba43bd9c7cf51a37928bc3")
-		.queryString("base", "USD") // Unirest bygger helst sin egen query string
-		.queryString("prettyprint", "true")
-		.asJson(); // Berätta för Unirest att vi får tillbaka JSON.
-		// JSON-objektet ligger gömt som en JsonNode i svaret
-		JsonNode json = response.getBody();
-		JSONObject currencyRates = json.getObject();
-		System.out.println(currencyRates.toString());
-		// Man måste stänga ner Unirest innan programmet avslutas
+		String apiKey = "19c47c8596ba43bd9c7cf51a37928bc3";
+		HttpResponse<JsonNode> response = Unirest.get("https://openexchangerates.org/api/latest.json")
+		.header("accept", "application/json")
+		.queryString("app_id", apiKey)
+		.queryString("base", "USD") //Default: USD
+		.queryString("prettyprint", "1") //Default: 1
+		.asJson();
 		Unirest.shutdown();
-		
+		JSONObject currencyRates = response.getBody().getObject();
 		return currencyRates;
 	}
-	
-	public static void main(String[] args) throws UnirestException, IOException {
-		fetchCurrencyRates();
-	}
+
 }
