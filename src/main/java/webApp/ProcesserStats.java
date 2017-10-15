f39214e3d59b1a529fbdb4ac4f78c5064ed20e85
oscar a <oscar093@live.se>
2017-10-15 13:09:27 +0200

    Nu finns det ett sammanställt Jsonobject

--- src/main/java/webApp/ProcesserStats.java
+++ src/main/java/webApp/ProcesserStats.java
@@ -15,128 +15,127 @@ import com.google.gson.reflect.TypeToken;
  * currency.
  */
 public class ProcesserStats {
-	private static Gson gson = new Gson();
-	private FileReader fr = new FileReader();
-	private String marketJson;
-	private String currencyJson;
+  private static Gson gson = new Gson();
+  private FileReader fr = new FileReader();
+  private String marketJson;
+  private String currencyJson;
 
-	private String currentCurrency = "2.00";
+  private String currentCurrency = "0.000208886471";//Just nu hur mycket en dollar kostar i btc. 
+//  1 USD = 0.000208886471 BTC
+//  1 BTC = USD/BTC = 4787.28941
 
-	public ProcesserStats() {
-		try {
-			this.marketJson = fr.readFile("files/MarketData.txt");
-			this.currencyJson = fr.readFile("files/currencyRates.json");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  public ProcesserStats() {
+    try {
+      this.marketJson = fr.readFile("files/MarketData.txt");
+      this.currencyJson = fr.readFile("files/currencyRates.json");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	/**
-	 * Get all market data as Market array.
-	 * 
-	 * @return Market[]
-	 */
-	public Market[] getMarketGsonBeans() {
-		Market[] data = gson.fromJson(marketJson, Market[].class);
-		return data;
-	}
+  /**
+   * Get all market data as Market array.
+   * 
+   * @return Market[]
+   */
+  public Market[] getMarketGsonBeans() {
+    Market[] data = gson.fromJson(marketJson, Market[].class);
+    return data;
+  }
 
-	/**
-	 * Creates a map out of currency data.
-	 * 
-	 * @return Map
-	 */
-	public Map<String, String> getCurrencyHashMap() {
-		Type type = new TypeToken<Map<String, String>>() {
-		}.getType();
-		Map<String, String> myMap = new Gson().fromJson(currencyJson, type);
-		return myMap;
-	}
+  /**
+   * Creates a map out of currency data.
+   * 
+   * @return Map
+   */
+  public Map<String, String> getCurrencyHashMap() {
+    Type type = new TypeToken<Map<String, String>>() {
+    }.getType();
+    Map<String, String> myMap = new Gson().fromJson(currencyJson, type);
+    return myMap;
+  }
 
-	/**
-	 * Creates a JSONobject out of currency data.
-	 * 
-	 * @return
-	 */
-	public JSONObject getCurrencyJsonObject() {
-		JSONObject jo = new JSONObject(currencyJson);
-		return jo;
-	}
+  /**
+   * Creates a JSONobject out of currency data.
+   * 
+   * @return
+   */
+  public JSONObject getCurrencyJsonObject() {
+    JSONObject jo = new JSONObject(currencyJson);
+    return jo;
+  }
 
-	/**
-	 * Creates a JSONObject out of json formatted String.
-	 * 
-	 * @param jsonString
-	 * @return JSONObject
-	 */
-	public JSONObject stringToJsonObject(String jsonString) {
-		JSONObject jo = new JSONObject(jsonString);
-		return jo;
-	}
-	
-	/**
-	 * Marchaling the data and produce the final output
-	 * @return JSONArray
-	 */
-	public JSONArray finalData() {
-		String exchangeCurrency = this.currentCurrency;
-		Market[] data = this.getMarketGsonBeans();
-		JSONArray ja = new JSONArray();
-		for(Market mrkt : data) {
-			JSONObject js = new JSONObject();
-			if(mrkt.volume>0) {
-				double finalCurrency = mrkt.latest_trade / Double.parseDouble(exchangeCurrency);
-				js.put("market", mrkt.symbol);
-				js.put("last_price", finalCurrency);
-				ja.put(js);
-			}
-		}		
-		return ja;
-	}
-	
+  /**
+   * Creates a JSONObject out of json formatted String.
+   * 
+   * @param jsonString
+   * @return JSONObject
+   */
+  public JSONObject stringToJsonObject(String jsonString) {
+    JSONObject jo = new JSONObject(jsonString);
+    return jo;
+  }
+  
+  /**
+   * Marchaling the data and produce Json Array containing all markets
+   * final price in one currency.
+   * @return JSONArray
+   */
+  public JSONArray finalData(String cc) {
+    Market[] data = this.getMarketGsonBeans();
+    JSONArray ja = new JSONArray();
+    for(Market mrkt : data) {
+      JSONObject js = new JSONObject();
+      if(mrkt.volume>0) {
+        double finalCurrency = btcCurrencyConverter(mrkt.close, mrkt.currency, cc); 
+        js.put("market", mrkt.symbol);
+        js.put("last_price", finalCurrency);
+        ja.put(js);
+      }
+    }   
+    return ja;
+  }
+  
   /**
    *Formats JsonObject to its final form.
-   *
-   *
+   *@return JSONObject
    */
-	public JSONObject finalData2() {
-		JSONObject jo = new JSONObject();
-		jo.put("markets", this.finalData().toString());
-		jo.put("Base", "SEK");
-		return jo;
-	}
+  public JSONObject finalData2(String currency) {
+    JSONObject jo = new JSONObject();
+    jo.put("markets", this.finalData(currency).toString());
+    jo.put("Base", "SEK");
+    return jo;
+  }
 
-	// Only for testing!
-	public static void main(String[] args) {
-		ProcesserStats p = new ProcesserStats();
-//		Market[] d = p.getMarketGsonBeans();
-//		for (Market da : d) {
-//			System.out.println(da.symbol);
-//			System.out.println(da.currency);
-//			System.out.println(da.latest_trade + "\n");
-//		}
-//		JSONObject m = p.getCurrencyJsonObject();
-//		for (Object key : m.keySet()) {
-//			String keyStr = (String) key;
-//			Object keyvalue = m.get(keyStr);
-//
-//			// Print key and value
-//			System.out.println("" + keyStr + ": " + keyvalue);
-//		}
-//
-//		System.out.println("\n==============================================\n");
-//
-//		String ratesStr = m.optString("rates");
-//		JSONObject currencies = p.stringToJsonObject(ratesStr);
-//
-//		for (Object key : currencies.keySet()) {
-//			String keyStr = (String) key;
-//			Object keyvalue = currencies.get(keyStr);
-//			System.out.println("" + keyStr + ": " + keyvalue);
-//		}
-		
-//		JSONArray ja = p.finalData();
-		JSONObject jo = p.finalData2();
-		System.out.println(jo.toString());
-	}
+  /**
+   * Converts between currencies
+   */
+  public double btcCurrencyConverter(double oneBTCinBeginCUR, String beginCUR, String finalCUR) {
+    JSONObject m = this.getCurrencyJsonObject();
+    String ratesStr = m.optString("rates");
+    JSONObject currencies = this.stringToJsonObject(ratesStr);
+    double finalPrice = -1;
+    double goalCURinUSD;
+    if (beginCUR.contentEquals(finalCUR)) {
+      finalPrice = oneBTCinBeginCUR;
+    } else if (beginCUR.equals("USD")) {
+      goalCURinUSD = (Double) currencies.get(finalCUR);
+      finalPrice = oneBTCinBeginCUR * goalCURinUSD;
+    } else if (finalCUR.equals("USD")) {
+      finalPrice = oneBTCinBeginCUR *(1/((Double) currencies.get(beginCUR)));
+    } else {
+      goalCURinUSD = (Double) currencies.get(finalCUR);
+      double beginCURinUSD = (Double) currencies.get(beginCUR);
+      double goalCURinBeginCUR = (1 / beginCURinUSD) * goalCURinUSD;
+      finalPrice = oneBTCinBeginCUR * goalCURinBeginCUR;
+    }
+    return finalPrice;
+  }
+
+  // Only for testing!
+  public static void main(String[] args) {
+    ProcesserStats p = new ProcesserStats();
+    String finalOutput = p.finalData2("DKK").toString();
+    System.out.println(finalOutput);
+  }
 }
