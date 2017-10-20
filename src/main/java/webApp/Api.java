0112aa81e1fc6d2f7d2ea1822cdd0e23db3a886f
kalle303 <karlparadis96@gmail.com>
2017-10-20 14:20:53 +0200

    API: Skicka rätt felkoder. Controller: Ta emot Map

--- src/main/java/webApp/Api.java
+++ src/main/java/webApp/Api.java
@@ -1,10 +1,19 @@
 package webApp;
 
 import static spark.Spark.port;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static spark.Spark.get;
 	
 public class Api {
 	private Controller cont;
+	private JSONObject errorInternal = new JSONObject("{ \"status\": 500,\"message\": \"500 Internal Server Error\" }");
+	private JSONObject errorNotFound = new JSONObject("{ \"status\": 404,\"message\": \"404 Not Found\" }");
 	
 	public Api(Controller cont) {
 		this.cont = cont;
@@ -14,19 +23,32 @@ public class Api {
 	public void run() {
 		port(5000);
 		
-		get("/marketPrices/:baseCurrency", "application/json", (request, response) -> {
+		get("/marketPrices/:baseCurrency", (request, response) -> {
 			response.type("application/json; charset=UTF-8");
 			response.status(200);
 			String baseCurrency = request.params(":baseCurrency");
 			System.out.println("Förfrågan kom: " + request.pathInfo());
-			return cont.getMarketPrices(baseCurrency.toUpperCase());
+			try {
+				return cont.getMarketPrices(baseCurrency.toUpperCase());
+			} catch (FileNotFoundException e) {
+				response.status(404);
+				return errorNotFound.toString(2);
+			} catch (JSONException | IOException e) {
+				response.status(500);
+				return errorInternal.toString(2);
+			}
 		});
 		
-		get("/volumes", "application/json", (request, response) -> {
+		get("/volumes", (request, response) -> {
 			response.type("application/json; charset=UTF-8");
 			response.status(200);
 			System.out.println("Förfrågan kom: " + request.pathInfo());
-			return cont.getBTCVolumeByCurrency();
+			try {
+				return cont.getBTCVolumeByCurrency();
+			} catch (JSONException | IOException e) {
+				response.status(500);
+				return errorInternal.toString(2);
+			}
 		});
 	}	
 }
--- src/main/java/webApp/Controller.java
+++ src/main/java/webApp/Controller.java
@@ -1,33 +1,23 @@
 package webApp;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.HashMap;
 
 public class Controller {
 
-	final long marketIntervall = 18^6; // Ersätt med antal timmar till uppdatering
-	final long currencyIntervall = 18^6; // Ersätt med antal timmar till uppdatering
-	private JSONObject errorCode = new JSONObject("{ \"key1\": \"value1\",\"key2\": \"value2\" }");
-	private JSONArray errorCodeArr = new JSONArray();
-
-
-	private Map<String, MarketPricesWrapper> marketPricesMappedByCurrency = new TreeMap<>();
+	private Map<String, JSONArray> marketPrices;
+	private long marketLastTimeUpdated;
 	private final long MARKET_INTERVAL_MILLIS = TimeUnit.HOURS.toMillis(5);
 	
 	private JSONObject volumeByCurrency;
-	private long volumeLastUpdated;
+	private long volumeLastTimeUpdated;
 	private final long VOLUME_INTERVAL_MILLIS = TimeUnit.HOURS.toMillis(5);
-	
-	
-	private class MarketPricesWrapper {
-		public JSONArray marketPrices;
-		public long lastTimeUpdated;
-	}
 
 	private ProcesserStats procesStats;
 	
@@ -40,46 +30,32 @@ public class Controller {
 	 * @param baseCurrency The base currency to display the price of the markets in.
 	 * @return A JSONObject.
 	 */
-	public JSONArray getMarketPrices(String baseCurrency) {
-		MarketPricesWrapper marketPrices = marketPricesMappedByCurrency.get(baseCurrency);
-		try {
-			if (marketPrices == null || marketPrices.lastTimeUpdated + MARKET_INTERVAL_MILLIS < System.currentTimeMillis()) {
-				System.out.println("Updating prices!");
-				marketPrices = new MarketPricesWrapper();
-				marketPrices.marketPrices = procesStats.marketMap().get(baseCurrency);
-				marketPrices.lastTimeUpdated = System.currentTimeMillis();
-				marketPricesMappedByCurrency.put(baseCurrency, marketPrices);
-			}
-		} catch (Exception e) {
-			if (marketPrices.marketPrices == null) {
-				return errorCodeArr;
-			}
+	public String getMarketPrices(String baseCurrency) throws FileNotFoundException, JSONException, IOException {
+		if (marketPrices == null || marketLastTimeUpdated + MARKET_INTERVAL_MILLIS < System.currentTimeMillis()) {
+			System.out.println("Updating marketPrices!");
+			marketPrices = procesStats.marketMap();
+			marketLastTimeUpdated = System.currentTimeMillis();
+		} else if (!marketPrices.containsKey(baseCurrency)) {
+			throw new FileNotFoundException(baseCurrency);	//404 Not Found
 		}
-		return marketPrices.marketPrices;
+		return marketPrices.get(baseCurrency).toString(2);
 	}
 
 	/**
 	 * Returnerar total BTC-handelsvolym per valuta sammantaget från flera marknader.
 	 * @return A JSONObject.
 	 */
-	public JSONObject getBTCVolumeByCurrency() {
-		try {
-			if (volumeByCurrency == null || volumeLastUpdated + VOLUME_INTERVAL_MILLIS < System.currentTimeMillis()) {
-				System.out.println("Updating volumes!");
-				volumeByCurrency = ProcessorVolume.getBTCVolumeByCurrency();
-				volumeLastUpdated = System.currentTimeMillis();
-			}
-		} catch (Exception e) {
-			if (volumeByCurrency == null) {
-				return errorCode;
-			}
+	public String getBTCVolumeByCurrency() throws JSONException, IOException {
+		if (volumeByCurrency == null || volumeLastTimeUpdated + VOLUME_INTERVAL_MILLIS < System.currentTimeMillis()) {
+			System.out.println("Updating volumes!");
+			volumeByCurrency = ProcessorVolume.getBTCVolumeByCurrency();
+			volumeLastTimeUpdated = System.currentTimeMillis();
 		}
-		return volumeByCurrency;
+		return volumeByCurrency.toString(2);
 	}
 
 	public static void main(String[] args) {
 		Controller cont = new Controller();
-    Api api = new Api(cont);
-
+		Api api = new Api(cont);
 	}
 }
